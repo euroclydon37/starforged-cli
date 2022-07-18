eedda3790b653992c5ffe16ae49eec0159b92577
Jeremy Dunn <jeremy.dunn@chime.com>
2022-07-17 23:14:16 -0500

    add move reference with text highlights

--- moves/index.js
+++ moves/index.js
@@ -1,85 +1,35 @@
 const prompts = require("prompts");
-const { toUpper, prop } = require("ramda");
-const moves = require("../moves.json");
+const { starforged } = require("dataforged");
+const { marked } = require("marked");
+const TerminalRenderer = require("marked-terminal");
+const chalk = require("chalk");
 
-const moveMap = {
-  Legacy: ["Advance", "Continue a Legacy", "Earn Experience"],
-  Adventure: [
-    "Aid Your Ally",
-    "Check Your Gear",
-    "Compel",
-    "Face Danger",
-    "Gather Information",
-    "Secure an Advantage",
-  ],
-  Fate: ["Ask the Oracle", "Pay the Price"],
-  Combat: [
-    "Battle",
-    "Clash",
-    "Enter the Fray",
-    "Face Defeat",
-    "Gain Ground",
-    "React Under Fire",
-    "Strike",
-    "Take Decisive Action",
-  ],
-  Session: [
-    "Begin a Session",
-    "Change Your Fate",
-    "End a Session",
-    "Set a Flag",
-    "Take a Break",
-  ],
-  Suffer: [
-    "Companion Takes a Hit",
-    "Endure Harm",
-    "Endure Stress",
-    "Lose Momentum",
-    "Sacrifice Resources",
-    "Withstand Damage",
-  ],
-  Exploration: [
-    "Confront Chaos",
-    "Explore a Waypoint",
-    "Finish an Expedition",
-    "Make a Discovery",
-    "Set a Course",
-    "Undertake an Expedition",
-  ],
-  Connection: [
-    "Develop Your Relationship",
-    "Forge a Bond",
-    "Make a Connection",
-    "Test Your Relationship",
-  ],
-  Threshold: ["Face Death", "Face Desolation", "Overcome Destruction"],
-  Quest: [
-    "Swear an Iron Vow",
-    "Fulfill Your Vow",
-    "Forsake Your Vow",
-    "Reach a Milestone",
-  ],
-  Recover: ["Heal", "Hearten", "Repair", "Resupply", "Sojourn"],
-};
+marked.setOptions({
+  renderer: new TerminalRenderer({
+    strong: chalk.green.bold,
+  }),
+});
 
 async function referenceAMove() {
-  const { move } = await prompts([
-    {
-      type: "autocomplete",
-      name: "category",
-      message: "Choose a category.",
-      choices: Object.keys(moveMap).map((value) => ({ title: value, value })),
-    },
-    {
-      type: "autocomplete",
-      name: "move",
-      message: "Choose a move.",
-      choices: (category) =>
-        moveMap[category].map((value) => ({ title: value, value })),
-    },
-  ]);
+  const { categoryIndex } = await prompts({
+    type: "select",
+    name: "categoryIndex",
+    message: "Choose a category.",
+    choices: starforged["Move Categories"].map((category) => category.Name),
+  });
 
-  console.log(prop(toUpper(move))(moves));
+  const { moveIndex } = await prompts({
+    type: "select",
+    name: "moveIndex",
+    message: "Choose a move.",
+    choices: starforged["Move Categories"][categoryIndex].Moves.map(
+      (move) => move.Name
+    ),
+  });
+
+  console.log(
+    marked(starforged["Move Categories"][categoryIndex].Moves[moveIndex].Text)
+  );
 }
 
 module.exports = { referenceAMove };
