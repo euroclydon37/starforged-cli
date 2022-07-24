f54d2d705c052c26f3244f006e7bc182a1d953e9
Jeremy Dunn <jeremy.dunn@chime.com>
2022-07-24 09:35:56 -0500

    Add event editing and deleting

--- commands/story.js
+++ commands/story.js
@@ -1,59 +1,86 @@
 const prompts = require("prompts");
 const { readDb, writeDb } = require("../db");
+const {
+  autocompletePromptByIndex,
+  getPropByPrompt,
+} = require("../userPrompts");
+const { remove, adjust } = require("ramda");
 
-const commands = {
-  logEvent: async () => {
-    const data = await readDb();
+async function logEvent() {
+  const data = await readDb();
 
-    if (!data.events) {
-      data.events = [];
-    }
+  if (!data.events) {
+    data.events = [];
+  }
 
-    const input = await prompts({
-      type: "text",
-      name: "text",
-      message: "What happened?",
-    });
-
-    data.events.push(input.text);
-
-    writeDb(data);
-  },
-  reviewEvents: async () => {
-    const data = await readDb();
-
-    const { count } = await prompts({
-      type: "number",
-      name: "count",
-      message: "How many events?",
-    });
-
-    const events = data.events ?? [];
-
-    console.log(`\n...\n\n${events.slice(count * -1).join("\n\n")}\n`);
-  },
-};
-
-async function story() {
-  const response = await prompts({
-    type: "autocomplete",
-    name: "command",
-    message: "And now what?",
-    choices: [
-      {
-        title: "Log an Event",
-        description: "Keep a history of what your character accomplishes.",
-        value: "logEvent",
-      },
-      {
-        title: "View an Event",
-        description: "Review the story up to this point.",
-        value: "reviewEvents",
-      },
-    ],
+  const input = await prompts({
+    type: "text",
+    name: "text",
+    message: "What happened?",
   });
 
-  commands[response.command]();
+  data.events.push(input.text);
+
+  writeDb(data);
+}
+
+async function reviewEvents() {
+  const data = await readDb();
+
+  const { count } = await prompts({
+    type: "number",
+    name: "count",
+    message: "How many events?",
+  });
+
+  const events = data.events ?? [];
+
+  console.log(`\n...\n\n${events.slice(count * -1).join("\n\n")}\n`);
+}
+
+async function editEvent() {
+  const data = await readDb();
+
+  const index = await autocompletePromptByIndex({
+    message: "Which event?",
+    choices: data.events,
+  });
+
+  const input = await prompts({
+    type: "text",
+    name: "text",
+    message: "What happened?",
+  });
+
+  data.events = adjust(index, () => input.text, data.events);
+  await writeDb(data);
+}
+
+async function deleteEvent() {
+  const data = await readDb();
+
+  const index = await autocompletePromptByIndex({
+    message: "Which event?",
+    choices: data.events,
+  });
+
+  data.events = remove(index, 1, data.events);
+  await writeDb(data);
+}
+
+async function story() {
+  const commands = {
+    logEvent,
+    reviewEvents,
+    editEvent,
+    deleteEvent,
+  };
+  const command = await getPropByPrompt({
+    message: "And now what?",
+    keyValueMap: commands,
+  });
+
+  await command();
 }
 
 module.exports = { story };
